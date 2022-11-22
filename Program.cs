5753dd6cc008b215d02edc8da3c5a47939905cd2
Will-Bee <78200129+Will-Bee@users.noreply.github.com>
2022-11-22 13:01:53 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+
+public class apiRequest
+{
+    public static string Get(string url)
+    {
+
+        var request = (HttpWebRequest)WebRequest.Create(url);
+
+        var response = (HttpWebResponse)request.GetResponse();
+
+        var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+        return responseString;
+
+    }
+}
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string url = "https://www.bartosek.cz/shared/testEnv/OFD-API-ESP8266/var.txt";
+        String state;
+
+
+        Console.WriteLine($"" +
+            $"Optic fiber detector - End user command line interface\n" +
+            $"\nFor more visit github/Will-Bee" +
+            $"\n" +
+            $"\nReading at url: {url}");
+
+        Console.WriteLine("\nSTATE:");
+
+
+        while (true)
+        {
+            state = apiRequest.Get(url);
+
+            if (state == "True")
+            {
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("\rON ");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\rOFF");
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+
+        }
+
+
+    }
+}
